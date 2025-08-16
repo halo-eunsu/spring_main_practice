a8018ad4bb0d0a4697f4add995445ab7b73d0732
Jeong Eun Su <ujd017@naver.com>
2025-08-16 13:37:22 +0900

    reference commit

---
+++ src/main/java/com/example/demo/dto/RequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.RestController;
+
+@Getter
+public class RequestDTO {
+    private String username;
+    
+}
