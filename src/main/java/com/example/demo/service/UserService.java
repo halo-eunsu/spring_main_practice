2793bb78f925a09341a7acd8baebabe7d0004c95
Jeong Eun Su <ujd017@naver.com>
2025-08-19 13:38:12 +0900

    commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,13 +1,21 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.RequestDTO;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.entity.User;
 
 
 public class UserService {
 
 
+    UserRepository userRepository;
+
     public Boolean createUser(RequestDTO requestDTO){
 
-        User request = request.from
+        User request = request.from();
+        Boolean iscreated = userRepository.save(user);
+
+        return iscreated;
+
     }
 }
