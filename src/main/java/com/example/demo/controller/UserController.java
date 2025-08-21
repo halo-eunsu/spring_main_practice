1989db9fe37895a35d7c32ea5524f3a744272d52
Jeong Eun Su <ujd017@naver.com>
2025-08-21 10:54:47 +0900

    commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 
 import com.example.demo.dto.RequestDTO;
+import com.example.demo.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 
+    UserService userService;
+
     @PostMapping("")
     public ResponseEntity<RequestDTO> create(@RequestBody RequestDTO request) {
-        UserResponseDto user = userService.save(request);
+        UserResponseDto user = userService.createUser(request);
         return ResponseEntity.ok(user);
     }
 
