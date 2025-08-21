6b203c387c58e1e2728df984996fa33068be36f2
Jeong Eun Su <ujd017@naver.com>
2025-08-21 15:25:41 +0900

    commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,12 +2,16 @@ package com.example.demo.service;
 
 import com.example.demo.dto.UserREquestDTO;
 import com.example.demo.dto.UserResponseDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    
+
     public UserResponseDTO createUser(UserREquestDTO request) {
+
+        
     }
 }
