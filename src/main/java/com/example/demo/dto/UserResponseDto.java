ccc336b167aa8b56c2bf9764d92e552e7f7aec3a
Jeong Eun Su <ujd017@naver.com>
2025-08-21 11:07:24 +0900

    commit

---
+++ src/main/java/com/example/demo/dto/UserResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.demo.dto;
+
+public class UserResponseDto {
+
+
+    private String name;
+    private String username;
+    private String password;
+    private Integer age;
+    private Job job;
+    private String specialty;
+
+}
